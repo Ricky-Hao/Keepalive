d55d6c0dc12c466bb8c1a9b8cbdd58597a59e932
Ricky-Hao <14084342+Ricky-Hao@users.noreply.github.com>
2024-03-03 01:02:07 +0800

    Add simple password

--- Keepalive.Web/Configs/ServiceConfig.cs
+++ Keepalive.Web/Configs/ServiceConfig.cs
@@ -5,11 +5,13 @@ namespace Keepalive.Web.Configs
         public required string Database { get; set; }
         public required SmtpConfig Email { get; set; }
         public required string Host { get; set; }
+        public required string Password { get; set; }
 
         public void Validate()
         {
             ArgumentNullException.ThrowIfNullOrWhiteSpace(Database);
             ArgumentNullException.ThrowIfNullOrWhiteSpace(Host);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(Password);
             Email.Validate();
         }
     }
